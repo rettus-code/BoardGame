f383bb20f3c294a652d7dde081cafab506d9e08a
rettusm <rettusm@wwu.edu>
2022-04-28 11:55:46 -0700

    game complete

--- Game.java
+++ Game.java
@@ -2,22 +2,24 @@ public class Game{
    public Dice gameDice = new Dice();
    public Board gameBoard = new Board();
    private int numPlayers;
-   private int startingPlayer;
    private int activePlayer;
    private Day currentDay = new Day();
    private SceneCard[] deck = new SceneCard[40];
+   private int winner;
    
    private Game(){};
    private Game(int numPlayers){
       this.numPlayers = numPlayers;
-      this.startingPlayer = determineStartingPlayer(numPlayers);
-      this.activePlayer = startingplayer;
+      this.activePlayer = determineStartingPlayer();
       this.deck = shuffleDeck(deck);
-   };
-   private int getActivePlayer(){
-      return activePlayer();
    }
-   private void setActivePlayer(){
+   public int determineStartingPlayer(){
+      return (int)(Math.random() * numPlayers) + 1;
+   }
+   private int getActivePlayer(){
+      return this.activePlayer;
+   }
+   private void updateActivePlayer(){
       if(activePlayer < numPlayers){
          activePlayer++;
       } else {
@@ -28,5 +30,8 @@ public class Game{
       /*deck = need to populate array here then randomize it, probably need to make
                each card object as we populate it.*/           
       return deck;
+   }
+   public int rollDice(){
+      return (int)(Math.random() * 6) + 1;
    }
 }
