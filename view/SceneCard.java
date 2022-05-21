3d8effe1897ad0a5a8b5f11527d66c79ed64d20b
rettusm <rettusm@wwu.edu>
2022-05-21 14:30:55 -0700

    reorganize

---
+++ view/SceneCard.java
@@ -0,0 +1,12 @@
+package view;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.imageio.ImageIO;
+import javax.swing.JLayeredPane;
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+import java.util.HashMap;
+public class SceneCard extends javax.swing.JLayeredPane{
+
+}
---
+++ view/SceneCardView.java
@@ -0,0 +1,13 @@
+package view;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.imageio.ImageIO;
+import javax.swing.JLayeredPane;
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+import java.util.HashMap;
+public class SceneCardView extends javax.swing.JLayeredPane{
+
+
+}
